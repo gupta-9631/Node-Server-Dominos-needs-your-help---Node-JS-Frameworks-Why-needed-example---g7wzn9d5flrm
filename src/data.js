5e7b708844814a994451bfd77bce835da956d0d3
Rahul <gupta-9631>
2022-09-27 23:18:47 +0530

    final

---
+++ src/data.js
@@ -0,0 +1,6 @@
+const data = {
+  phone: "18602100000",
+  email: "guestcaredominos@jublfood.com",
+};
+
+module.exports = data;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 var http = require("http");
+const data = require("./data");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "./welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to Dominos!");
+  } else if (req.url === "./contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 }
-
+httpServer.listen(8081);
 module.exports = httpServer;
+
