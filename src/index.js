fb533f51095a55ec1ee7315124aec63d2b5a7d6b
Rahul <gupta-9631>
2022-09-28 08:25:37 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const data = require("./data");
 const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
-  if (req.url === "./welcome") {
+  if (req.url === "/welcome") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Welcome to Dominos!");
-  } else if (req.url === "./contact") {
+  } else if (req.url === "/contact") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(data));
   } else {
